3a8f17da175eb1e743161174a11e987dfc30bd59
zedae <105038052+zedae@users.noreply.github.com>
2025-01-03 14:16:03 +0100

    Update connect.py

--- connect.py
+++ connect.py
@@ -3,7 +3,7 @@ import socket
 def client_connect(ipadd):
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((ipadd, 6000))  # Connect to server at localhost:5000
+    client.connect((ipadd, 6000))  # Connect to server at localhost:6000
     print("Connected to the server.")
 
 
